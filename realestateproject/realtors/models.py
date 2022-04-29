63239e8ce4384450bd78140f2f956bf5ae834bae
Griffins-sys254 <griffinesonyango@gmail.com>
2022-04-29 18:36:41 +0300

    Realtors models push

--- realestateproject/realtors/models.py
+++ realestateproject/realtors/models.py
@@ -1,3 +1,28 @@
+from distutils.command import upload
+import email
+from pyexpat import model
+from unicodedata import name
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+class Realtor(models.Model):
+    name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    description = models.TextField(blank=True)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=70)
+    is_mvp = models.BooleanField(default=False)
+    hire_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+class LandLord(models.Model):
+    pass
+
+
+class Hireguy(models.Model):
+    pass
