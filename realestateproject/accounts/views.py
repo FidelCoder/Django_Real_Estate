4dd6cc792c2942a90cc29b6f3eebecdb06a2c2bb
Griffins-sys254 <griffinesonyango@gmail.com>
2022-05-02 01:13:52 +0300

    final made big changes and adjustments on accounts.views

--- realestateproject/accounts/views.py
+++ realestateproject/accounts/views.py
@@ -1,4 +1,5 @@
 from distutils.command.build_scripts import first_line_re
+from multiprocessing import context
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
@@ -26,4 +27,35 @@ def register(register):
                 if User.objects.filter(email=email).exists():
                     messages.error(request, 'That email exists in the database')
                     return redirect('register')
-                else
+                else:
+                    user = User.objects.create_user(username,password=password,email=email,first_name=first_name,last_name=last_name)
+                    user.save()
+                    messages.success(request, 'You are now registered and can log in')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+    else:
+        return render(request, 'acounts/register.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if User is not None:
+            auth.login(request, user)
+            messages.success(request,'You are now logged in')
+            return redirect('dashboard')
+        else:
+            return render(request, 'account/login.html')
+
+def dashboard(request):
+    user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+
+    context = {
+        'contacts':user_contacts
+    }
+    return render(request, 'accounts/dashboard.html', context)
